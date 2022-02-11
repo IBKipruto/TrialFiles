94cd7997173845fccdc7eb389b2020a681c8e61f
IBKipruto <isaacbrian254@gmail.com>
2022-02-11 08:56:48 +0300

    Update 2.1

---
+++ loop.php
@@ -0,0 +1,21 @@
+<?php
+$num1=1;
+while ($num1<=8) {
+    echo "You're position :$num1 <br>";
+
+$num1++;
+}
+//do while loop
+$num2= 1;
+
+do {
+    echo "The number is :$num2<br>";
+    $num2++;
+}
+while ($num2<=8);
+//for loop
+$mpesa=1;
+for ($mpesa=0; $mpesa<= 25; $mpesa++) {
+    echo "Your Mpesa balance is : $mpesa  you're broke<br>";
+}
+?>
